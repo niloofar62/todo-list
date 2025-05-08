0014468f6a1962d1f197a309ef054bfdb51a70dc
Niloofar Nayebi <nil_nyb@yahoo.com>
2025-05-07 22:52:49 -0400

    week 07 completed

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,10 +1,13 @@
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
   const [isEditing, setIsEditing] = useState(false);
   const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  useEffect(() => {
+    setWorkingTitle(todo.title);
+  }, [todo]);
 
   function handleEdit(e) {
     setWorkingTitle(e.target.value);
@@ -55,3 +58,4 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
 }
 
 export default TodoListItem;
+
